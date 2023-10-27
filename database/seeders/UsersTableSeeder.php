a96ff74d54d0eaa47a514b38ad78092cec0a2475
Dieter Burgstaller <72778774+burgdie@users.noreply.github.com>
2023-10-27 07:03:17 +0200

    107 Seed Demo User Data - S18 Multi Auth with Breeze Create Auth for User/Vendor/Admin

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        DB: table(users)->insert([
+            //Admin
+            [
+                'name' => 'Admin',
+                'userName' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+                'status' => 'active',
+
+            ],
+            //Vendor
+            [
+                'name' => 'Vendor',
+                'userName' => 'vendor',
+                'email' => 'vendor@gmail.com',
+                'password' => Hash::make('password'),
+                'role' => 'vendor',
+                'status' => 'active',
+
+            ],
+            //User or Customer
+            [
+                'name' => 'User',
+                'userName' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('password'),
+                'role' => 'user',
+                'status' => 'active',
+
+            ],
+        ]);
+    }
+}
