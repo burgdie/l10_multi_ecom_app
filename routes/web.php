b1b6fc726685789ec698798e66198a10581dd8a1
Dieter Burgstaller <72778774+burgdie@users.noreply.github.com>
2023-10-28 04:29:28 +0200

    110 Laravel Multi Auth with Breeze Part 3 - S18 Multi AUth with Breeze Create Auth for User/Vendor/Admin

--- routes/web.php
+++ routes/web.php
@@ -31,13 +31,18 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
 /**
  * Admin Routes
  */
-Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+});
 
 /**
  * Vendor Routes
  */
+Route::middleware(['auth', 'role:vendor'])->group(function () {
 
-Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
+    Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
+});
