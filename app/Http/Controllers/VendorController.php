1056c15fb8ad314abef5662cc48489558efd0a1b
Dieter Burgstaller <72778774+burgdie@users.noreply.github.com>
2023-10-28 03:54:33 +0200

    109 Laravel Multi Authwith Breeze Part 2 - S18 Multi AUth with Breeze Create Auth for User/Vendor/Admin

---
+++ app/Http/Controllers/VendorController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class VendorController extends Controller
+{
+    public function VendorDashboard()
+    {
+        return view('vendor.vendor_dashboard');
+    }
+}
