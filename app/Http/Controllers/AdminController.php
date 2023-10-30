0f971fd2868656390bfbc5bc18046e37923524ce
Dieter Burgstaller <72778774+burgdie@users.noreply.github.com>
2023-10-30 04:02:46 +0100

    118 Admin Profile & Image Update Part 1 - S19 Project Admin Panel Setup

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -28,5 +29,15 @@ class AdminController extends Controller
 
         return redirect('/admin/login');
 
+    }
+    /**
+     * Admin Profile
+     */
+    public function AdminProfile()
+    {
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+
+        return view('admin.admin_profile_view', compact('adminData'));
     }
 }
